f9e33e3a4a4311734127cf68c393341812cfdd07
anacafe-oss <ana.cafe@escola.pr.gov.br>
2026-08-10 08:19:36 -0300

    wuvdgjhdsv

---
+++ script.js
@@ -0,0 +1,20 @@
+// Banco de dados de curiosidades rápidas
+const curiosidades = [
+    "A música 'Bohemian Rhapsody' do Queen levou 3 semanas para ser gravada na época!",
+    "Ouvir música alta enquanto se exercita pode aumentar seu rendimento físico em até 15%.",
+    "A canção mais antiga gravada no mundo chama-se 'Hino Hurrita nº 6' e foi gravada em placas de argila.",
+    "O som das plantas não existe, mas cientistas já criaram sintetizadores que convertem sinais elétricos delas em música!",
+    "Espaço sideral: Astronautas já levaram violões para a Estação Espacial Internacional para tocar em gravidade zero."
+];
+
+const textoCuriosidade = document.getElementById('curiosidade-texto');
+const btnCuriosidade = document.getElementById('btn-curiosidade');
+
+// Evento ao clicar no botão
+btnCuriosidade.addEventListener('click', () => {
+    // Escolhe uma frase aleatória
+    const indiceAleatorio = Math.floor(Math.random() * curiosidades.length);
+    
+    // Altera o texto na tela
+    textoCuriosidade.textContent = `"${curiosidades[indiceAleatorio]}"`;
+});
